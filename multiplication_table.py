3097782d677edfb2bf3fccbbf47ceff5413b1198
Tirth Bharatiya <emailtirth@gmail.com>
2018-08-24 17:09:34 +0530

    This programme is to find multiplication table of any number

---
+++ multiplication_table.py
@@ -0,0 +1,13 @@
+for i in range(1,11):
+    print(f"1*{i}={1*i}")
+def print_multiplication_table(a,b):
+    for i in range(1,b+1):
+        print(f"{a}*{i}={a*i}")
+
+def print_multiplication(a,b):
+    for i in range(1,b+1):
+        print("{0}*{1}={2}".format(a,i,a*i))
+
+
+
+
