b9cc1545e1ccc864a47f2e80743d3e6f75230450
Tirth Bharatiya <emailtirth@gmail.com>
2018-12-08 00:28:36 +0530

    understand some maths function and how to import them

---
+++ maths_fun.py
@@ -0,0 +1,35 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+>>> 
+>>> import math as m
+>>> 
+>>> x = m.sqrt(25)
+>>> x
+5.0
+>>> 
+>>> x = m.floor(2.9)
+>>> x
+2
+>>> 
+>>> x = m.ceil(2.2)
+>>> x
+3
+>>> 
+>>> x = m.pi
+>>> x
+3.141592653589793
+>>> 
+>>> x = m.e
+>>> x
+2.718281828459045
+>>> 
+>>> from math import sqrt,pow
+>>> x = pow(2,3)
+>>> x
+8.0
+>>> 
+>>> x = int(pow(2,3))
+>>> x
+8
+>>> 
