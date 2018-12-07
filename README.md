aaf33b941618864bf0bf1fc788d3c5476297db11
Tirth Bharatiya <emailtirth@gmail.com>
2018-12-08 00:13:13 +0530

    understand how address work in python

--- README.md
+++
@@ -1,2 +0,0 @@
-# python-examples
-Python programmes for beginner
---
+++ address.py
@@ -0,0 +1,15 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> a=5
+>>> b=a
+>>> id(a)
+140717892818080
+>>> id(b)
+140717892818080
+>>> k=5
+>>> id(k)
+140717892818080
+>>> a=10
+>>> id(a)
+140717892818240
+>>> 
