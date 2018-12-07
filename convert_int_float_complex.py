c5f964c76e682d3935a95dac5d01f6c3df5691d5
Tirth Bharatiya <emailtirth@gmail.com>
2018-12-08 00:15:54 +0530

    understand how to conver integer into float and float into integer

---
+++ convert_int_float_complex.py
@@ -0,0 +1,17 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> a=5.6
+>>> b=9
+>>> x=int(a)
+>>> x
+5
+>>> y=complex(a,b)
+>>> y
+(5.6+9j)
+>>> a=float(b)
+>>> a
+9.0
+>>> a=int(a)
+>>> a
+9
+>>> 
