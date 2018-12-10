d26b75904523312fec205fbf6ec30fea380f1418
Tirth Bharatiya <emailtirth@gmail.com>
2018-12-10 22:44:15 +0530

    some operation upon string

---
+++ string.py
@@ -0,0 +1,18 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> name = 'tirth'
+>>> name + 'Bharatiya'
+'tirthBharatiya'
+>>> name[0]
+'t'
+>>> name[-1]
+'h'
+>>> name[1:4]
+'irt'
+>>> name[:4]
+'tirt'
+>>> name[2:]
+'rth'
+>>> len(name)
+5
+
