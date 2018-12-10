20d3a84978c4d7f7ddf6aa8fec7993b7a54abb36
Tirth Bharatiya <emailtirth@gmail.com>
2018-12-10 22:57:45 +0530

    understand about tuple

---
+++ tuple.py
@@ -0,0 +1,11 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> tup = (21,3.6,'tirth')
+>>> tup[1]
+3.6
+>>> tup[1] = 2.6
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    tup[1] = 2.6
+TypeError: 'tuple' object does not support item assignment
+>>> 
