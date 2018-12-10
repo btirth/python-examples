e26050e4964fdd5b0d7377d667b87c53a5bfe1c2
Tirth Bharatiya <emailtirth@gmail.com>
2018-12-10 22:55:27 +0530

    relational, logical and boolean operators

---
+++ rel_logi_bool_oper.py
@@ -0,0 +1,24 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> a=6
+>>> b,c = 8,6
+>>> 
+>>> a<b
+True
+>>> a==b
+False
+>>> 
+>>> a<8 or b>5
+True
+>>> a<8 and b>5
+True
+>>> a>8 and b>5
+False
+>>> 
+>>> x = True
+>>> not x
+False
+>>> x = not x
+>>> x
+False
+>>> b
