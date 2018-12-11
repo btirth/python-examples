d507124d11255041b96e31115908a14d427415d8
Tirth Bharatiya <emailtirth@gmail.com>
2018-12-11 22:18:57 +0530

    pass value using default argument

---
+++ default_arg.py
@@ -0,0 +1,14 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> def person(name,age=28):
+	print('name is ',name)
+	print('age is ',age)
+
+	
+>>> person('tirth')
+name is  tirth
+age is  28
+>>> person('tirth',20)
+name is  tirth
+age is  20
+>>> 
