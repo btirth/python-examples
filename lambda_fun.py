7095317e284e12fdf6516914962209a80a321228
Tirth Bharatiya <emailtirth@gmail.com>
2018-12-12 23:55:23 +0530

    lambda/anonymous function

---
+++ lambda_fun.py
@@ -0,0 +1,14 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> f = lambda a:a*a
+>>> x = f(5)
+>>> x
+25
+>>> 
+>>> 
+>>> 
+>>> f = lambda a,b:a+b
+>>> x = f(5,6)
+>>> x
+11
+>>> 
