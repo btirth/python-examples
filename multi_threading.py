2a02ba572158ac0404b59f61d5e5128473d2b316
Tirth Bharatiya <emailtirth@gmail.com>
2018-12-16 20:58:38 +0530

    to understand multi threading

---
+++ multi_threading.py
@@ -0,0 +1,30 @@
+from time import sleep
+from threading import *
+
+class hello(Thread):
+
+    def run(self):
+        print("Hello")
+        sleep(1)
+
+
+class hi(Thread):
+
+    def run(self):
+        print("Hi")
+        sleep(1)
+
+
+
+t1 = hello()
+t2 = hi()
+
+t1.start()
+sleep(0.2)
+
+t2.start()
+
+t1.join()
+t2.join()
+
+print("Bye")
